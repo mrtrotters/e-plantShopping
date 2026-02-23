9c332a596f55e48de9e0c5f06b62308c390c3fdd
sam <samtrott94@gmail.com>
2026-02-23 20:29:31 +0000

    23.2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) {
+        existingItem.quantity++; //if item exists in cart already, up the quantity.
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 }); // if the item does not exist in the array/cart, add it and make its quantity 1.
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+      
+      const itemToUpdate = state.items.find((item) => item.name === name);// Find the item in the cart that matches the given name
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+      }
     },
   },
 });
